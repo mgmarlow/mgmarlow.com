abf465a9e3eb2226128a7dde468f93fbcce8c7b1
Graham Marlow <graham@onesignal.com>
2021-09-26 11:28:57 -0700

    Better article previews

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -1,5 +1,14 @@
 const ArticlePreview = ({ article }) => {
-  return <a href={article.url}>{article.title}</a>
+  return (
+    <div className="columns">
+      <div className="column is-2 is-hidden-mobile">
+        {article.date.substring(0, 10)}
+      </div>
+      <div className="column">
+        <a href={article.url}>{article.title}</a>
+      </div>
+    </div>
+  )
 }
 
 export default ArticlePreview
