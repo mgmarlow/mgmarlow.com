25c7750010c9e2d954c7f6a2e82d6357901d8960
Graham Marlow <mgmarlow@hey.com>
2022-01-30 10:44:15 -0800

    Format words index

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'public/*': '/' })
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj)
-      .toLocaleString(DateTime.DATE_MED)
-      .toLowerCase()
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
   })
 }
