2074ccbf67c21b44d7aac16e720926755cc72133
Graham Marlow <mgmarlow@hey.com>
2022-01-29 18:02:34 -0800

    Add date filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
+const { DateTime } = require('luxon')
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'public/*': '/' })
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj)
+      .toLocaleString(DateTime.DATE_MED)
+      .toLowerCase()
+  })
 }
