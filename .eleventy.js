7fc3f93806220f32a8e38fa2c67f8748485d948c
Graham Marlow <graham@mgmarlow.com>
2023-01-16 08:11:26 -0800

    Add collections tab

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,21 @@ module.exports = function (eleventyConfig) {
   })
 
   eleventyConfig.addFilter('excerpt', (post) => {
-    const content = post.replace(/(<([^>]+)>)/gi, "");
-    return content.substr(0, content.lastIndexOf(" ", 200)) + "...";
+    const content = post.replace(/(<([^>]+)>)/gi, '')
+    return content.substr(0, content.lastIndexOf(' ', 200)) + '...'
+  })
+
+  eleventyConfig.addCollection('allTags', async (api) => {
+    const items = await api.getAll()
+
+    return items
+      .flatMap((i) => i.data.tags)
+      .filter((tag, i, self) => {
+        if (!tag || tag === 'post') {
+          return false
+        }
+
+        return self.indexOf(tag) === i
+      })
   })
 }
