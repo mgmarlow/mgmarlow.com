a44664dd4a4d5125de9a41f73fca9b78907cb444
Graham Marlow <graham@mgmarlow.com>
2025-02-12 15:44:41 -0800

    Trying out crazy things

--- _11ty/filters/excerpt.js
+++ _11ty/filters/excerpt.js
@@ -1,9 +1,9 @@
 import striptags from 'striptags'
 
-export default function excerpt(post) {
+export default function excerpt(post, n = 200) {
   return striptags(post)
     .replace(/&quot;/g, '"')
-    .substring(0, 200)
+    .substring(0, n)
     .trim()
     .concat('...')
 }
